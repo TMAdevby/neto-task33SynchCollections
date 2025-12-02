7e1a66295f45fa724e7d14f39377c0ef1d0383d3
TMAdevby <mtipun@mail.ru>
2025-12-02 12:18:48 +0300

    Blocking

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,17 +3,73 @@ package ru.netology;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
+    public static ArrayBlockingQueue aQueue = new ArrayBlockingQueue(100);
+    public static ArrayBlockingQueue bQueue = new ArrayBlockingQueue(100);
+    public static ArrayBlockingQueue cQueue = new ArrayBlockingQueue(100);
+
     public static void main(String[] args) {
-        String[] texts = new String[25];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("aab", 30_000);
+
+        Thread generateThread = new Thread(() ->{
+            String text = "";
+            for (int i = 0; i < 10_000; i++) {
+                text = generateText("abc", 100_000);
+                try {
+                    aQueue.put(text);
+                    System.out.println("put");
+                    bQueue.put(text);
+                    cQueue.put(text);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        generateThread.start();
+
+        Thread countAthread = new Thread(() -> {
+            String text = "";
+            String textInMemory = "";
+            int aMax = 0;
+            for (int i = 0; i < 10_000; i++) {
+                try {
+                    text = (String) aQueue.take();
+                } catch (InterruptedException e) {
+                    return;
+                }
+                int aCount = 0;
+                for (int j = 0; j < text.length(); j++) {
+                    if (text.charAt(j) == 'a') {
+                        aCount++;
+                    }
+                }
+                if (aMax < aCount) {
+                    aMax = aCount;
+                    textInMemory = text;
+                }
+            }
+            System.out.println("'a' max -> " + textInMemory + "\n встречается " + aMax + " раз");
+        });
+
+        countAthread.start();
+        try {
+            generateThread.join();
+            countAthread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        
+
+        System.out.println("Программа завершена.");
+
+
     }
+
+
+
+
 
     public static String generateText (String letters,int length){
         Random random = new Random();
@@ -22,7 +78,5 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
-    }
-}
     }
 }
