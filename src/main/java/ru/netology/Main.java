916bbd8813bf15e2a4eb8e9f735ba6ab26c4e8df
TMAdevby <mtipun@mail.ru>
2025-12-02 12:46:22 +0300

    Blocking3

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -13,15 +13,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Thread generateThread = new Thread(() ->{
+        Thread generateThread = new Thread(() -> {
+            System.out.println("generateThread стартовал");
             String text = "";
             for (int i = 0; i < 10_000; i++) {
                 text = generateText("abc", 100_000);
                 try {
                     aQueue.put(text);
-                    System.out.println("put");
+                    System.out.println("putA");
                     bQueue.put(text);
+                    System.out.println("putB");
                     cQueue.put(text);
+                    System.out.println("putC");
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -31,12 +34,14 @@ public class Main {
         generateThread.start();
 
         Thread countAthread = new Thread(() -> {
+            System.out.println("countAthread стартовал");
             String text = "";
             String textInMemory = "";
             int aMax = 0;
             for (int i = 0; i < 10_000; i++) {
                 try {
                     text = (String) aQueue.take();
+                    System.out.println("takeA");
                 } catch (InterruptedException e) {
                     return;
                 }
@@ -51,27 +56,80 @@ public class Main {
                     textInMemory = text;
                 }
             }
-            System.out.println("'a' max -> " + textInMemory + "\n встречается " + aMax + " раз");
+            String out = textInMemory.substring(0, 100);
+            System.out.println("'a' max -> " + out + "\n встречается " + aMax + " раз");
+        });
+
+        Thread countBthread = new Thread(() -> {
+            System.out.println("countBthread стартовал");
+            String text = "";
+            String textInMemory = "";
+            int bMax = 0;
+            for (int i = 0; i < 10_000; i++) {
+                try {
+                    text = (String) bQueue.take();
+                    System.out.println("takeB");
+                } catch (InterruptedException e) {
+                    return;
+                }
+                int bCount = 0;
+                for (int j = 0; j < text.length(); j++) {
+                    if (text.charAt(j) == 'b') {
+                        bCount++;
+                    }
+                }
+                if (bMax < bCount) {
+                    bMax = bCount;
+                    textInMemory = text;
+                }
+            }
+            String out = textInMemory.substring(0, 100);
+            System.out.println("'b' max -> " + out + "\n встречается " + bMax + " раз");
+        });
+
+        Thread countCthread = new Thread(() -> {
+            System.out.println("countCthread стартовал");
+            String text = "";
+            String textInMemory = "";
+            int cMax = 0;
+            for (int i = 0; i < 10_000; i++) {
+                try {
+                    text = (String) cQueue.take();
+                    System.out.println("takeC");
+                } catch (InterruptedException e) {
+                    return;
+                }
+                int aCount = 0;
+                for (int j = 0; j < text.length(); j++) {
+                    if (text.charAt(j) == 'c') {
+                        aCount++;
+                    }
+                }
+                if (cMax < aCount) {
+                    cMax = aCount;
+                    textInMemory = text;
+                }
+            }
+            String out = textInMemory.substring(0, 100);
+            System.out.println("'c' max -> " + out + "\n встречается " + cMax + " раз");
         });
 
         countAthread.start();
+        countBthread.start();
+        countCthread.start();
+
         try {
             generateThread.join();
             countAthread.join();
+            countBthread.join();
+            countCthread.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
         System.out.println("Программа завершена.");
-
-
     }
 
-
-
-
-
-    public static String generateText (String letters,int length){
+    public static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
         for (int i = 0; i < length; i++) {
