261b53d028bcb083c25d441be230e27da95e2a60
TMAdevby <mtipun@mail.ru>
2025-12-02 11:20:50 +0300

    Initial

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+        
+    }
+
+    public static String generateText (String letters,int length){
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
+    }
+}
